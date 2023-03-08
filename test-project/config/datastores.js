3cf4d5d2b691391c40e9a538c2bfdc59c837815f
Sigurjón Vídalín Lýðsson <sigurjonl@users.noreply.github.com>
2023-03-08 14:44:45 +0100

    db additions

--- test-project/config/datastores.js
+++ test-project/config/datastores.js
@@ -48,8 +48,8 @@
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-     adapter: 'sails-mysql',
-     url: 'mysql://user:password@host:port/database',
+     //adapter: 'sails-mysql',
+     //url: 'mysql://user:password@host:port/database',
     
     
   },
