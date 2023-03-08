b61efa597bd7a6229c6123a17f9b714e77e7c0a9
Sigurjón Vídalín Lýðsson <sigurjonl@users.noreply.github.com>
2023-03-08 14:09:40 +0100

    database changes

--- test-project/config/datastores.js
+++ test-project/config/datastores.js
@@ -50,7 +50,8 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
-
+    adapter: require('sails-mysql'),
+    url: 'mysql://root:squ1ddy@localhost:3306/my_dev_db_name',
   },
 
 
